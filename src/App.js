a32a75443f582aba40cdeda287f16dd0507df8d3
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-03-18 21:27:48 +0530

    add usestate and useeffect hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "./App.css";
+import { Container, Row, Col } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [details, setDetails] = useState({});
+
+  const fetchDetails = async () => {
+    const { data } = await Axios.get("https://randomuser.me/api/");
+    console.log(data);
+
+    const details = data.results[0];
+    setDetails(details);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, []);
+  return <div></div>;
+};
 
 export default App;
